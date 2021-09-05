20c4a36f4de7283b061a29b3a5b70ccc1cc8e93d
Justin <jmp86@yahoo.com>
2021-09-04 17:57:14 -1000

    commit tree b2731179cba21775fb01385a6f65a25ccd952116

---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+    def first_name=(name)
+        @firstname = name
+    end
+    def first_name
+        @firstname
+    end
+    def last_name=(name)
+        @lastname = name
+    end
+    def last_name
+        @lastname
+    end
+
+
+end
